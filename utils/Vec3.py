48ae252c67e38e009611164f840697bc69866a9f
Platholl <cyprien.plateauholleville@gmail.com>
2020-04-16 22:19:45 +0200

    Initialize math utils class.

---
+++ utils/Vec3.py
@@ -0,0 +1,15 @@
+class Vec3:
+    def __init__(self, x=0.0, y=0.0, z=0.0):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __add__(self, other):
+        return Vec3(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __mul__(self, other):
+        return Vec3(
+            (self.y * other.z) - (self.z * other.y),
+            (self.z * other.x) - (self.x * other.z),
+            (self.x * other.y) - (self.y * other.x)
+        )
