f0324a4dfc8e40c58a46342228ac034633fe526e
Nicolas Lépy <33573107+nicolasLepy@users.noreply.github.com>
2020-04-22 13:45:14 +0200

    Adding tree libraries to main

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import objects.Torus
 import objects.IsoSurfaceGenerator
 import scenes.torus
 import scenes.isosurface
+import scenes.tree
 import objects.Materials
+import objects.Tree
 
 
 def reload_modules_main():
@@ -14,10 +16,12 @@ def reload_modules_main():
     importlib.reload(objects.Torus)
     importlib.reload(objects.IsoSurfaceGenerator)
     importlib.reload(objects.Materials)
+    importlib.reload(objects.Tree)
     importlib.reload(scenes.torus)
     importlib.reload(scenes.isosurface)
+    importlib.reload(scenes.tree)
 
 
 def main():
     reload_modules_main()
-    scenes.isosurface.isosurface()
+    scenes.tree.tree()
