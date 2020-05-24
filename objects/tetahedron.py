d712f888ebee011f05b50c133be6f1d3134566cf
Platholl <cyprien.plateauholleville@gmail.com>
2020-05-24 11:02:41 +0200

    Remove old tetahedron code samples

--- objects/tetahedron.py
+++ objects/tetahedron.py
@@ -8,6 +8,12 @@ import bmesh
 
 
 class Tetrahedron3:
+
+    def __init__(self, scale):
+        self.__location = (0.0, 0.0, 0.0)
+        self.__scale = scale
+        self.__vertices = []
+
     # Define the four points of the triangle
     def calculate_vertices(self):
         """
@@ -58,68 +64,3 @@ class Tetrahedron3:
         for vert in self.__vertices:
             res.append((vert[0] + self.__location[0], vert[1] + self.__location[1], vert[2] + self.__location[2]))
         return res
-
-    def __init__(self, scale):
-        self.__location = (0.0, 0.0, 0.0)
-        self.__scale = scale
-        self.__vertices = []
-
-class Tetahedron:
-    def __init__(self, depth=6, size=2):
-        self.__depth = depth
-        self.__size = size
-
-        self.faces = []
-
-    def build_mesh(self):
-        first_triangle = [Vector((0, 0, 0)),
-                          Vector((0, self.__size, 0)),
-                          Vector((self.__size, self.__size/2., 0))]
-
-        second_triangle = [Vector((self.__size / 3., self.__size / 2., self.__size)),
-                           Vector((0, 0, 0)),
-                           Vector((self.__size, self.__size / 2., 0))]
-
-        third_triangle = [Vector((self.__size / 3., self.__size / 2., self.__size)),
-                          Vector((0, self.__size, 0)),
-                          Vector((self.__size, self.__size / 2., 0))]
-
-        fourth_triangle = [Vector((self.__size / 3., self.__size / 2., self.__size)),
-                           Vector((0, self.__size, 0)),
-                           Vector((0, 0, 0))]
-
-        self.depth_triangle(first_triangle,  self.__depth)
-        self.depth_triangle(second_triangle, self.__depth)
-        self.depth_triangle(third_triangle,  self.__depth)
-        self.depth_triangle(fourth_triangle, self.__depth)
-
-
-    def depth_triangle(self, triangle, depth):
-        p1 = triangle[0]
-        p2 = triangle[1]
-        p3 = triangle[2]
-
-        mid_point_v1_v2 = Vector(((p1.x + p2.x) / 2.,
-                                  (p1.y + p2.y) / 2.,
-                                  (p1.z + p2.z) / 2.))
-        mid_point_v1_v3 = Vector(((p1.x + p3.x) / 2.,
-                                  (p1.y + p3.y) / 2.,
-                                  (p1.z + p3.z) / 2.))
-        mid_point_v2_v3 = Vector(((p3.x + p2.x) / 2.,
-                                  (p3.y + p2.y) / 2.,
-                                  (p3.z + p2.z) / 2.))
-
-        subtriangle1 = [p1, mid_point_v1_v2, mid_point_v1_v3]
-        subtriangle2 = [p2, mid_point_v1_v2, mid_point_v2_v3]
-        subtriangle3 = [p3, mid_point_v1_v3, mid_point_v2_v3]
-
-        if depth == 0:
-            self.faces.extend([subtriangle1, subtriangle2, subtriangle3])
-        else:
-            depth = depth - 1
-            self.depth_triangle(subtriangle1, depth)
-            self.depth_triangle(subtriangle2, depth)
-            self.depth_triangle(subtriangle3, depth)
-
-    def get_faces(self):
-        return self.faces
--- scenes/tetahedron.py
+++ scenes/tetahedron.py
@@ -5,49 +5,6 @@ from objects.tetahedron import Tetrahedron3
 import objects.Materials
 
 
-def tetahedron():
-    mesh = bpy.data.meshes.new("tetahedron_mesh")  # add a new mesh
-    obj = bpy.data.objects.new("tetahedron", mesh)  # add a new object using the mesh
-
-    scene = bpy.context.scene
-
-    bpy.context.collection.objects.link(obj)
-    bpy.context.view_layer.objects.active = obj  # set as the active object in the scene
-
-    bpy.data.objects["tetahedron"].select_set(True)
-
-    mesh = bpy.context.object.data
-    bm = bmesh.new()
-
-    t = Tetahedron()
-    t.build_mesh()
-    faces = t.get_faces()
-
-    for i in range(0, len(faces)):
-        v1 = bm.verts.new(faces[i][0])  # add a new vert
-        v2 = bm.verts.new(faces[i][1])  # add a new vert
-        v3 = bm.verts.new(faces[i][2])  # add a new vert
-
-        bm.faces.new((v1, v2, v3))
-
-    # Recalculate normal.
-    bm.normal_update()
-
-    # make the bmesh the object's mesh
-    bm.to_mesh(mesh)
-    bm.free()  # always do this when finished
-    values = [True] * len(mesh.polygons)
-
-    objects.Materials.SmoothColor((0., 0., 0., 0.)).apply_material(obj)
-    bpy.data.objects['Camera'].location = [0, 0, 10]
-    utils.BlenderUtils.update_camera(bpy.data.objects['Camera'],
-                                     focus_point=obj.location,
-                                     distance=11.53)
-
-    scene.render.resolution_x = 1920
-    scene.render.resolution_y = 1920
-
-
 def generate_iteration(objects, scale):
     new_objects = []
 
@@ -64,6 +21,8 @@ def generate_iteration(objects, scale):
 
 
 def fractal_tetrahedron():
+    scene = bpy.context.scene
+
     scale = 4
     initial_tetrahedron = Tetrahedron3(scale)
 
@@ -116,3 +75,11 @@ def fractal_tetrahedron():
     # make the bmesh the object's mesh
     bm.to_mesh(mesh)
     bm.free()  # always do this when finished
+    objects.Materials.SmoothColor((0., 0., 0., 0.)).apply_material(obj)
+    bpy.data.objects['Camera'].location = [0, 0, 10]
+    utils.BlenderUtils.update_camera(bpy.data.objects['Camera'],
+                                     focus_point=obj.location,
+                                     distance=11.53)
+
+    scene.render.resolution_x = 1920
+    scene.render.resolution_y = 1920
